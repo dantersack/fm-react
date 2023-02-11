8d94ba0e2557afb5446416e1697321f943249d91
Dante Sack <sack_dante@hotmail.com>
2023-02-11 15:52:14 -0300

    setup accident-counter project

---
+++ accident-counter/src/components/application.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from './counter';
+
+const Application = () => (
+  <main className="flex h-full place-content-center items-center bg-cyan-900">
+    <Counter />
+  </main>
+);
+
+export default Application;
---
+++ accident-counter/src/components/counter.tsx
@@ -0,0 +1,21 @@
+const Counter = () => {
+  return (
+    <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
+      <h1>Days Since the Last Accident</h1>
+      <p className="text-6xl">0</p>
+      <div className="flex gap-2">
+        <button>➖ Decrement</button>
+        <button>🔁 Reset</button>
+        <button>➕ Increment</button>
+      </div>
+      <div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input type="number" value={0} />
+          <button type="submit">Update Counter</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
