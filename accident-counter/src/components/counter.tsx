7847192584d00f54a23a10988678f10a4c0629b5
Dante Sack <sack_dante@hotmail.com>
2023-02-11 16:52:30 -0300

    typing component state

--- accident-counter/src/components/counter.tsx
+++ accident-counter/src/components/counter.tsx
@@ -1,16 +1,34 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [draftCount, setDraftCount] = useState(0);
+
   return (
     <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => setCount((prevState) => prevState - 1)}>
+          ➖ Decrement
+        </button>
+        <button onClick={() => setCount(0)}>🔁 Reset</button>
+        <button onClick={() => setCount((prevState) => prevState + 1)}>
+          ➕ Increment
+        </button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCount(draftCount);
+          }}
+        >
+          <input
+            type="number"
+            value={draftCount}
+            onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
